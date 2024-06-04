b9fab1a8add177c75ee0501e641eefa90f867d05
DulinPerera <115730490+DulinPerera@users.noreply.github.com>
2024-06-04 18:05:33 +0530

    created Model & Routes

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import restaurantRoutes from "./routes/restaurantsRoutes.js"
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+
+
+app.use(express.json());
+
+mongoose.connect(
+    process.env.MONGO
+).then(()=>{
+    console.log('MongoDb is connected')
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
+
+app.use('/restaurants', restaurantRoutes);

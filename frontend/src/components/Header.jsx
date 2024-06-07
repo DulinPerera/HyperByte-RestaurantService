607d2165feaa56841098773f48536f84d6690aa7
DulinPerera <115730490+DulinPerera@users.noreply.github.com>
2024-06-07 12:34:11 +0530

    Link to HyperByte Page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "flowbite-react";
+
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -6,9 +6,11 @@ function Header() {
     <div>
       <nav className="bg-white border-b border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center  mx-auto p-4 ">
+        <Link to= "https://hyperbyte.io/" target="_blank">
           <div>
             <span className='px-2 py-1 bg-gradient-to-r from-blue-600 via-blue-400 to-blue-200 rounded-lg text-white'>hyper://</span>byte
         </div>
+        </Link>
       </div>
     </nav>
   </div>

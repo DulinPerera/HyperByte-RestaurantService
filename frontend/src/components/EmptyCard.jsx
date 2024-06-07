75ec8989c6e3bf6499591dbea002965a7a8653bf
DulinPerera <115730490+DulinPerera@users.noreply.github.com>
2024-06-07 12:12:20 +0530

    small changes

--- frontend/src/components/EmptyCard.jsx
+++ frontend/src/components/EmptyCard.jsx
@@ -3,7 +3,7 @@ import emptyImg from '../assets/images.png'
 const EmptyCard = () => {
   return (
     <div className='flex flex-col items-center justify-center mt-20'>
-        <img src={emptyImg} alt="" />
+        <img src={emptyImg} alt="Empty list img"  />
         <p className='w-1/2 text font-medium text0slate-700 text-center leading-7 mt-5'>No Restaurants Found</p>
 
     </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,14 +4,10 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div>
-    
-
-
-
-  <nav className="bg-white border-gray-200 dark:bg-gray-900">
-    <div className="max-w-screen-xl flex flex-wrap items-center  mx-auto p-4 ">
-        <div>
-          <span className='px-2 py-1 bg-gradient-to-r from-blue-600 via-blue-400 to-blue-200 rounded-lg text-white'>hyper://</span>byte
+      <nav className="bg-white border-b border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center  mx-auto p-4 ">
+          <div>
+            <span className='px-2 py-1 bg-gradient-to-r from-blue-600 via-blue-400 to-blue-200 rounded-lg text-white'>hyper://</span>byte
         </div>
       </div>
     </nav>
